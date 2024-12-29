154964b2eae0018521dab2b01978f0d7d735c43c
Mamat1411 <muhammadmujahid14@gmail.com>
2024-12-29 13:08:04 +0700

    [Feat] Change Theme from Pinky to Sky Blue

--- resources/js/script.js
+++ resources/js/script.js
@@ -8,7 +8,7 @@ simplyCountdown('.simply-countdown', {
   year: 2025, // required
   month: 2, // required
   day: 1, // required
-  hours: 8, // Default is 0 [0-23] integer
+  hours: 9, // Default is 0 [0-23] integer
   words: {
     //words displayed into the countdown
     days: { singular: 'hari', plural: 'hari' },
